9c572a0737dad84a79a2f28be8fee3157800a41c
Agboola Abdulkabir <pro.kaiserlautern@gmail.com>
2022-09-19 15:00:36 +0100

    $f added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/*
+ * This program prings
+ * exactly 'Programming is like building a multilingual
+ * puzzle
+ *
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	
+	return (0);
+
+}
