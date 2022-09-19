06c99aee719e24325600810f4591eaf5c162a17a
Agboola Abdulkabir <pro.kaiserlautern@gmail.com>
2022-09-19 18:05:36 +0100

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char char_type;
+	int int_type;
+	long long_type;
+	float float_type;
+
+	printf("Size of a char: %ld byte(s)\n", sizeof(char_type));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int_type));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long_type));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long_type));
+	printf("Size of an float: %ld byte(s)\n", sizeof(float_type));
+
+	return (0);
+}
