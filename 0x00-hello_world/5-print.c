d6a46290a9fd54f460b6df30f36f809c60afabf8
Agboola Abdulkabir <pro.kaiserlautern@gmail.com>
2022-09-19 16:43:45 +0100

    5-print.c

---
+++ 0x00-hello_world/5-print.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * This program is meant to print using the printf
+ * function without a replacement by puts or any 
+ * alternative 
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
