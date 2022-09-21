2c63e7ef658fd765ac366eba5c2e88db6485dc47
Agboola Abdulkabir <pro.kaiserlautern@gmail.com>
2022-09-21 23:15:35 +0100

    /home/kolawole/alx-low_level_programming/0x01-variables_if_else_while/2-print_alphabet.c: added

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Main function (Entry point)
+ *
+ * Documentation: print lowercase english alphabets
+ * with only `putchar`
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lettr;
+
+	for (; lettr <= 'z'; lettr++)
+	{
+		putchar(lettr);
+	}
+	putchar('\n');
+
+	return (0);
+}
