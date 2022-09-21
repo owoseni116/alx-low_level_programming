c3f0ac905c0aa992b34dccd8157e67dc75a8531a
Agboola Abdulkabir <pro.kaiserlautern@gmail.com>
2022-09-21 12:44:34 +0100

    101-quote.c: something

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+/**
+ * main - Main function
+ *
+ * Documentation: Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19\"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char word[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, word, 60);
+
+
+	return (0);
+}
