de6db28b4821bc856a93b8b7d779597220ba612b
Agboola Abdulkabir <pro.kaiserlautern@gmail.com>
2022-09-22 12:44:12 +0100

    0-putchar.c: added

---
+++ 0x01-variables_if_else_while/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ *
+ * Documentation:prints '_putchar\n'
+ *
+ * Return: Always zero (Success)
+ */
+int main(void)
+{
+	puts("_putchar");
+
+	return (0);
+}
