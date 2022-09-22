db307af7ba73dcfe64984a142f21e1249bb7cdd6
Agboola Abdulkabir <pro.kaiserlautern@gmail.com>
2022-09-22 23:18:05 +0100

    10-add.c: added

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - add two numbers and return
+ * the result
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: a + b, int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
