5e54e681a64bafe1c0a264f6e0490b553e4f019c
Agboola Abdulkabir <pro.kaiserlautern@gmail.com>
2022-09-22 18:07:27 +0100

    1-alphabet.c: added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print alphabet - prints the alphabet
+ * in lowercase followed by a new line
+ *
+ * Documentation: print alphabet
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char ltr = 'a';
+
+	for (; ltr <= 'z'; ltr++)
+	{
+		_putchar(ltr);
+	}
+	_putchar('\n');
+}
